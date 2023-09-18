c63121bb65ab50a7e4b12e3e8cde1b01a8cac2db
깃rhkddhks18 <taengja18@naver.com>
2023-09-18 11:10:34 +0900

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,11 @@
 package org.example;
 
-import java.time.Clock;
-import java.util.Collections;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
         System.out.printf("Hello and welcome!");
-
-
         for (int i = 1; i <= 5; i++) {
 
-
-            // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
         }
     }
